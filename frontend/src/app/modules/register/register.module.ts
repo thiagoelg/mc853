3137ab75e3574f09ae99a5e0f52d39b22babc423
Carlos Vieira <carlosadrianovieira@gmail.com>
2020-05-22 22:13:18 -0300

    feat(register): first register working

--- frontend/src/app/modules/register/register.module.ts
+++ frontend/src/app/modules/register/register.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from 'src/app/app-material/app-material.module';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
 
-
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [
-    CommonModule,
-    RegisterRoutingModule
-  ]
+  imports: [CommonModule, RegisterRoutingModule, AppMaterialModule, ReactiveFormsModule],
 })
-export class RegisterModule { }
+export class RegisterModule {}
