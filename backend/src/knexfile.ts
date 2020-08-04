379c8c45d8870e883cac395054ace7727d01f960
Thiago Lugli <thiagoelg@gmail.com>
2020-08-04 01:51:53 -0300

    Prod builds

--- backend/src/knexfile.ts
+++ backend/src/knexfile.ts
@@ -1,31 +1,33 @@
 import { Config } from "knex";
 require("ts-node/register");
 
+const config = {
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    extension: "ts",
+    tableName: "knex_migrations",
+    directory: "migrations",
+  },
+  seeds: {
+    extension: "ts",
+    directory: "seeds",
+  }
+} as Config;
+
 const knexConfig = {
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      extension: "ts",
-      tableName: "knex_migrations",
-      directory: "migrations",
-    },
-    seeds: {
-      extension: "ts",
-      directory: "seeds",
-    },
-  } as Config,
-  production: {} as Config,
+  development: config,
+  production: config
 };
 
 export = knexConfig;
