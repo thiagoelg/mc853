e909904124f9fbf95f73f4a6b10ec42bca7af724
Thiago Lugli <thiagoelg@gmail.com>
2020-08-04 14:47:28 -0300

    More stuff

--- frontend/src/app/modules/forms/forms-question-create/forms-question-create.component.ts
+++ frontend/src/app/modules/forms/forms-question-create/forms-question-create.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
+import { Question } from 'src/app/models/question';
 import { ResponseType } from 'src/app/models/responseType';
 import { FormsService } from '../forms.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, UrlSegment } from '@angular/router';
 
 @Component({
   selector: 'app-forms-question-create',
@@ -12,14 +13,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./forms-question-create.component.css'],
 })
 export class FormsQuestionCreateComponent implements OnInit {
+  @Output() created = new EventEmitter<Question>();
   form: FormGroup;
 
   responseTypes$: Observable<ResponseType[]>;
   types$: Observable<{ groupName: string; types: ResponseType[] }[]>;
+  currentPath: string;
 
-  constructor(private fb: FormBuilder, private formService: FormsService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private formService: FormsService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.buildForm();
     this.setTypeObservables();
+    this.route.parent.url.subscribe(url => {
+      this.currentPath = url[0].path;
+      console.log(url);
+    });
   }
 
   ngOnInit(): void { }
@@ -33,15 +45,18 @@ export class FormsQuestionCreateComponent implements OnInit {
 
   onSubmit() {
     const form = this.form.value;
+    console.log({ form });
 
-    this.formService.createQuestion(form).subscribe({
-      next: (data) => {
-        this.router.navigate(['/forms/questions/list']);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
+    // this.formService.createQuestion(form).subscribe({
+    //   next: (data) => {
+    //     console.log({ data });
+    //     this.created.emit(data);
+    //     this.router.navigate(['/forms/questions/list']);
+    //   },
+    //   error: (error) => {
+    //     console.error(error);
+    //   }
+    // });
   }
 
   setTypeObservables() {
