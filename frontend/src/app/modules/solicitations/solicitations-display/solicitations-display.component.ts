4dc67068e844420f2139f245e27b8032cc2986f1
Fellipe S. Scarpa Caetano <fellipe.caetano4@gmail.com>
2020-08-05 00:50:21 -0300

    Update solicitations-display.component.ts
    Add action to "Atribuit para mim" button

--- frontend/src/app/modules/solicitations/solicitations-display/solicitations-display.component.ts
+++ frontend/src/app/modules/solicitations/solicitations-display/solicitations-display.component.ts
@@ -7,17 +7,27 @@ import { SolicitationsService } from '../solicitations.service';
 @Component({
   selector: 'app-solicitations-display',
   templateUrl: './solicitations-display.component.html',
-  styleUrls: ['./solicitations-display.component.css']
+  styleUrls: ['./solicitations-display.component.css'],
 })
 export class SolicitationsDisplayComponent implements OnInit {
+  solicitationId: number;
   solicitation$: Observable<Solicitation>;
 
-  constructor(private solicitationsService: SolicitationsService, private route: ActivatedRoute) {
-  }
+  constructor(private solicitationsService: SolicitationsService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get('solicitation_id'));
-    this.solicitation$ = this.solicitationsService.fetchSolicitation(id);
+    this.solicitationId = Number(this.route.snapshot.paramMap.get('solicitation_id'));
+    this.solicitation$ = this.solicitationsService.fetchSolicitation(this.solicitationId);
   }
 
+  onAssignToUser() {
+    this.solicitationsService.assignToUser(this.solicitationId).subscribe({
+      next: (data) => {
+        console.log({ data });
+      },
+      error: (error) => {
+        console.log({ error });
+      },
+    });
+  }
 }
