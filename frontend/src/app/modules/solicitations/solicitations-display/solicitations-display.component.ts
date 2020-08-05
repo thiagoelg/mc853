abd4d1ca30dbed5d46b332205aef0c298097468c
Fellipe S. Scarpa Caetano <fellipe.caetano4@gmail.com>
2020-08-05 01:04:22 -0300

    Hide "Atribuir para mim" button when appropriate

--- frontend/src/app/modules/solicitations/solicitations-display/solicitations-display.component.ts
+++ frontend/src/app/modules/solicitations/solicitations-display/solicitations-display.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject, BehaviorSubject, merge } from 'rxjs';
 import { Solicitation } from 'src/app/models/solicitation';
 import { SolicitationsService } from '../solicitations.service';
+import { AuthService } from 'src/app/security/auth.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-solicitations-display',
@@ -11,18 +13,36 @@ import { SolicitationsService } from '../solicitations.service';
 })
 export class SolicitationsDisplayComponent implements OnInit {
   solicitationId: number;
+
+  canAssignToUser$: Observable<boolean>;
+
   solicitation$: Observable<Solicitation>;
 
-  constructor(private solicitationsService: SolicitationsService, private route: ActivatedRoute) {}
+  solicitationAssignedToUser$: Subject<Solicitation> = new Subject();
+
+  constructor(
+    private solicitationsService: SolicitationsService,
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.solicitationId = Number(this.route.snapshot.paramMap.get('solicitation_id'));
-    this.solicitation$ = this.solicitationsService.fetchSolicitation(this.solicitationId);
+
+    this.solicitation$ = merge(
+      this.solicitationsService.fetchSolicitation(this.solicitationId),
+      this.solicitationAssignedToUser$
+    );
+
+    this.canAssignToUser$ = this.solicitation$.pipe(
+      map((solicitation) => solicitation.managed_by_user_id !== this.authService.user.id)
+    );
   }
 
   onAssignToUser() {
     this.solicitationsService.assignToUser(this.solicitationId).subscribe({
       next: (data) => {
+        this.solicitationAssignedToUser$.next(data);
         console.log({ data });
       },
       error: (error) => {
