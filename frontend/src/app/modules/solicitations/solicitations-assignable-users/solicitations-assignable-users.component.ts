34d150c02ab87650c1a50296c6ef60605850e1ac
Fellipe S. Scarpa Caetano <fellipe.caetano4@gmail.com>
2020-08-05 10:24:39 -0300

    Create "UserRow" out of User fields

--- frontend/src/app/modules/solicitations/solicitations-assignable-users/solicitations-assignable-users.component.ts
+++ frontend/src/app/modules/solicitations/solicitations-assignable-users/solicitations-assignable-users.component.ts
@@ -2,17 +2,34 @@ import { OnInit, Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user';
 import { SolicitationsService } from '../solicitations.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   templateUrl: './solicitations-assignable-users.component.html',
   styleUrls: ['./solicitations-assignable-users.component.css'],
 })
 export class SolicitationsAssignableUsersComponent implements OnInit {
-  users$: Observable<User[]>;
+  rows$: Observable<UserRow[]>;
+
+  columnNames = {
+    id: 'Número',
+    name: 'Nome',
+    roleName: 'Perfil de acesso',
+  };
 
   constructor(private solicitationsService: SolicitationsService) {
-    this.users$ = solicitationsService.fetchAssignableUsers();
+    this.rows$ = solicitationsService.fetchAssignableUsers().pipe(
+      map((users) =>
+        users.map((user) => ({
+          id: user.id,
+          name: user.name,
+          roleName: user.role?.name,
+        }))
+      )
+    );
   }
 
   ngOnInit() {}
 }
+
+type UserRow = Pick<User, 'id' | 'name'> | { roleName?: string };
