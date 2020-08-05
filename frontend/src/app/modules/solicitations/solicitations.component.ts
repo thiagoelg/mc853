3db43d2c03d4536fd82ba69d1665ea3aebfee9aa
Fellipe S. Scarpa Caetano <fellipe.caetano4@gmail.com>
2020-08-04 22:56:57 -0300

    Add list of solicitations component

--- frontend/src/app/modules/solicitations/solicitations.component.ts
+++ frontend/src/app/modules/solicitations/solicitations.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { SolicitationsService } from './solicitations.service';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Solicitation } from 'src/app/models/solicitation';
 
 @Component({
   selector: 'app-solicitations',
   templateUrl: './solicitations.component.html',
-  styleUrls: ['./solicitations.component.css']
+  styleUrls: ['./solicitations.component.css'],
 })
 export class SolicitationsComponent implements OnInit {
+  unassignedSolicitations$: Observable<Solicitation[]> = new BehaviorSubject([]);
+  solicitationsManagedByUser$: Observable<Solicitation[]> = new BehaviorSubject([]);
+  solicitationsSubmittedByUser$: Observable<Solicitation[]> = new BehaviorSubject([]);
 
-  constructor() { }
+  constructor(private solicitationsService: SolicitationsService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
